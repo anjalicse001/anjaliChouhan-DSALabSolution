90e4055a601a62760c32a82a6a9e1e95026fb29b
anjali chouhan <anjalicse001@gmail.com>
2022-02-14 18:04:24 +0530

    modified

--- BalancingBracket/src/com/balancingbracket/driver/Main.java
+++ BalancingBracket/src/com/balancingbracket/driver/Main.java
@@ -1,10 +1,23 @@
 package com.balancingbracket.driver;
 
+import com.balancingbracket.service.BalancingBracket;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String bracketExpression = "([[{}]])";
+		Boolean result;
+		//result = checkingBracketsBalanced(bracketExpression);
+		BalancingBracket object = new BalancingBracket();
+		result =object.checkingBracketsBalanced(bracketExpression);
+		//result = checkingBracketsBalanced(bracketExpression);
+		
+		if (result)
+			System.out.println("The entered String has Balanced");
+		else 
+			System.out.println("Unbalanced");
 	}
 
+	
 }
--- BalancingBracket/src/com/balancingbracket/service/BalancingBracket.java
+++ BalancingBracket/src/com/balancingbracket/service/BalancingBracket.java
@@ -1,5 +1,46 @@
 package com.balancingbracket.service;
 
+
+
+import java.util.Stack;
 public class BalancingBracket {
+
+	//function to check if Brackets are balanced
+	public  boolean checkingBracketsBalanced(String bracketExpression)
+	{
+		Stack<Character> stack = new Stack<>();
+		for (int i=0;i<bracketExpression.length();i++)
+		{
+			char character = bracketExpression.charAt(i);
+			if (character =='(' || character=='[' || character=='}') {
+				stack.push(character);
+				continue;
+			}
+			if(stack.isEmpty())
+				return false;
+			
+			char c;
+			switch (character) {
+			case '}':
+				c = stack.pop();
+				if (c=='('|| c=='[')
+					return false;
+				break;
+			case ')':
+				c = stack.pop();
+				if(c=='{'|| c=='[')
+					return false ;
+				break;
+			case ']':
+				c= stack.pop();
+				if(c=='('||c=='{')
+					return false;
+				break;
+			}
+		}
+			return stack.isEmpty();
+		
+	}
+	
 
 }
